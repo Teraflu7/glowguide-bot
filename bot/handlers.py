973a0ca79a5653e85c4551b702c042f5de98cf8d
merey1112 <merey20081211@gmail.com>
2026-05-15 23:50:50 +0500

    Plan bot handlers and keyboards

---
+++ bot/handlers.py
@@ -0,0 +1,16 @@
+commands = [
+    "/start",
+    "/help",
+    "/skintype",
+    "/routine",
+    "/ingredient",
+    "/favorites"
+]
+
+# Описание команд
+# /start -> welcome message
+# /help -> bot instructions
+# /skintype -> choose skin type
+# /routine -> morning/evening skincare routine
+# /ingredient -> information about skincare ingredients
+# /favorites -> saved recommendations
