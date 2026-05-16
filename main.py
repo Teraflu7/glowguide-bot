43949f55d38abae491ab30cc87068b540a0ebeef
mukie <tamiris.2008@icloud.com>
2026-05-16 22:56:40 +0500

    Configure Telegram bot startup

--- main.py
+++ main.py
@@ -1,6 +1,32 @@
-def main():
-    print("GlowGuide Bot project started!")
+from aiogram import Bot, Dispatcher
+from aiogram.filters import Command
+from aiogram.types import Message
+
+from dotenv import load_dotenv
+import asyncio
+import os
+
+load_dotenv()
+
+TOKEN = os.getenv("BOT_TOKEN")
+
+bot = Bot(token=TOKEN)
+dp = Dispatcher()
+
+
+@dp.message(Command("start"))
+async def start_handler(message: Message):
+    await message.answer(
+        "Привет! ✨\n"
+        "Я GlowGuide Bot.\n"
+        "Я помогу подобрать уход по типу кожи 💖"
+    )
+
+
+async def main():
+    print("Bot started...")
+    await dp.start_polling(bot)
 
 
 if __name__ == "__main__":
-    main()
+    asyncio.run(main())
