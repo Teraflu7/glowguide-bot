3cac2becceb55d6dcce21d38585d598d7196dcc7
merey1112 <merey20081211@gmail.com>
2026-05-17 22:38:00 +0500

    Add help commands

--- bot/handlers.py
+++ bot/handlers.py
@@ -8,3 +8,13 @@ async def start_message(message: Message):
         "Привет! ✨\n"
         "Выбери тип кожи ниже 💖"
     )
+    async def help_message(message: Message):
+        await message.answer(
+"GlowGuide Bot ✨\n\n"
+"Используй кнопки, чтобы выбрать тип кожи 💖\n\n"
+"Бот показывает:\n"
+"- skincare рекомендации\n"
+"- утренний уход\n"
+"- вечерний уход\n"
+"- полезные советы"
+)
